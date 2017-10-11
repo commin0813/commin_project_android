aa495aeb49a3cbb6372f095efed7aaa5d823ab45
commin0813 <commin0813@gmail.com>
2017-10-11 17:01:09 +0900

    commit -- 4

---
+++ DiseaseManagement/app/src/main/java/commin/pro/diseasemanagement/page/management/Page2Management.java
@@ -0,0 +1,15 @@
+package commin.pro.diseasemanagement.page.management;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import commin.pro.diseasemanagement.R;
+
+public class Page2Management extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2_management);
+    }
+}
